652c0fffc5a6f3630a3100e8b14245bf83eba63c
Catalin Pintea <seven@192-168-0-104.rdsnet.ro>
2020-12-10 17:53:30 +0200

    Add SVG support, basic blocks

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'stylesheets')],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  },
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Block from '../images/svg/block.svg';
 
 function HomePage() {
+  const nBlocks = 3 * 2;
+
+  let blocks = [...Array(nBlocks)].map((e, i) => <Block />);
+
   return <>
     <h1>Labaketzi.</h1>
+    <div className="blocks">
+      { blocks }
+    </div>
   </>
 }
 
