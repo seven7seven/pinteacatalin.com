d744c0baa8276e0ab90f49b513b4fe5d5a769528
Catalin Pintea <seven@Catalins-MacBook-Pro.local>
2021-01-14 18:42:14 +0200

    Cleanup

--- pages/api/getphotos.js
+++ pages/api/getphotos.js
@@ -6,9 +6,6 @@ export default (req, res) => {
   const dir = path.resolve('./public', photoDirectory)
   const filenames = fs.readdirSync(dir)
   const photos = filenames.map(name => path.join('/', photoDirectory, name))
-
-  console.log('asdasd', filenames);
-
   res.statusCode = 200
-  res.json(photos);
+  res.json(photos)
 }
